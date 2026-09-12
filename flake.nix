0814212a1c2e4a90ddb24f70b9550d3f0160c5a3
minusKou <32330840+minusKou@users.noreply.github.com>
2026-09-12 17:30:43 +0800

    feat(flake): add chaotic-nyx source

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "CopraSense Flake Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, chaotic, ... }: {
+    nixosConfigurations = {
+      coprasense = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          chaotic.nixosModules.default
+        ];
+      };
+    };
+  };
+}
