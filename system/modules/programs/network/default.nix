df20b6da01eb33b5930bc08503a94f98d10dafd4
minusKou <32330840+minusKou@users.noreply.github.com>
2026-09-16 22:57:56 +0800

    fix: static ip and disable networkmanager

--- system/modules/programs/network/default.nix
+++ system/modules/programs/network/default.nix
@@ -1,10 +1,19 @@
 {
   networking = {
     hostName = "coprasense";
-    networkmanager.enable = true;
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 ];
     };
+    interfaces.eno1.ipv4.addresses = [{
+      address = "10.0.0.86";
+      prefixLength = 29;
+    }];
+    defaultGateway =  {
+      address = "10.0.0.81";
+      interface = "eno1";
+    };
+    nameservers = [ "10.0.0.66" ];
   };
+  services.openssh.enable = true;
 }
